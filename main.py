ca77ea9b45e06506331c1e310a360836d7e04a08
Patrick Mörtenböck <p.moertenboeck@gmail.com>
2017-07-17 14:54:54 +0200

    added more functionality
    mulit-page for commission and non-commission

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# from jinja2 import Template, Environment, PackageLoader, select_autoescape
-#
-# template = Template('template.html')
-# print template.render(items=([{'amount':1,'text':'hallo'},{'amount':2,'text':'hallo 2'},{'amount':3,'text':'hallo 3'}]), a_variable='here')
-#
-
 import os
 import jinja2
 import codecs
@@ -48,42 +42,53 @@ def read_source(src):
 	ids = list()
 
 	page_ids = list()
-	page_ids.append(0)
+	page_ids.append(start)
 
-	for i,d in enumerate(data[start:]):
-		if i//MAX_LINES >= len(page_ids):
 
-			if not d.startswith('#'):
-				page_ids.append(ids[-1])
-			else:
-				page_ids.append(i)
-				
+	data_len = len(data) - 1
+
+
+	prove_comm = None
+	for d in data[start:]:
 		if d.startswith('#'):
-			ids.append(i)
+			prove_comm = True
+			break
+
+
+	if prove_comm:
+		i = start
+		while i<data_len:
+			if i//MAX_LINES >= len(page_ids):
+
+				if not data[i].startswith('#'):
+					page_ids.append(ids[-1])
+					i = ids[-1]-1
+				else:
+					page_ids.append(i)
+
+			if data[i].startswith('#'):
+				if i not in ids:
+					ids.append(i)
+
+			i += 1
+			print('i:',i)
+	else:
+		for i,d in enumerate(data[start:]):
+			if i//MAX_LINES >= len(page_ids):
+				page_ids.append(i)
+
 
 	ids.append(len(data)-start-1)
-
-
-	# for i,id in enumerate(ids):
-	# 	print(id//MAX_LINES,len(page_ids))
-	# 	if id // MAX_LINES >= (len(page_ids)):
-	# 		page_ids.append(id)
-
-	data_len = len(data)-1
-
-	print(page_ids)
-	print(ids)
 
 	pages_list = list()
 	page_sum_before = 0.0
 	for i,id in enumerate(page_ids):
-		next_id = page_ids[i+1]+start if (i+1)<len(page_ids) else data_len
-		print(id,next_id)
+		next_id = page_ids[i+1] if (i+1)<len(page_ids) else data_len
 		page = {}
 		items = list()
 		page_sum = 0.0
 
-		for d in data[id+start:next_id]:
+		for d in data[id:next_id]:
 			if d.startswith('#'):
 				items.append({'name': d[1:].rstrip()})
 			else:
@@ -102,21 +107,6 @@ def read_source(src):
 	context['pages'] = pages_list
 
 	discount = data[len(data)-1]
-
-	# data_len = len(data)-1
-	#
-	# pages_list = list()
-	#
-	# items_outer_list = list()
-	# for i,id in enumerate(ids):
-	# 	next_id = ids[i+1] if (i+1)<len(ids) else data_len
-	# 	item = {}
-	# 	item['name'] = data[id][1:].rstrip()
-	# 	item['items'] = [{h: v.rstrip() for (h,v) in zip(heads,d.split(','))} for d in data[ids[i]+1:next_id]]
-	#
-	# 	items_outer_list.append(item)
-	#
-	# context['items'] = items_outer_list
 
 	if discount != '0':
 		context['discount'] = discount
@@ -150,77 +140,6 @@ def get_total_price(context):
 
 	return ('netto',netto),('mwst',mwst),('brutto',brutto)
 
-context = {
-	'pages':[
-		{'items':
-				[
-				{'name': 'Kommission 1'},
-				{'amount':5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
-				{'amount':5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount':5,'unit':'lfm','text':'ziehen','price_per_unit':2,'price':10},
-				{'name': 'Kommission 2'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
-				{'name': 'Kommission 3'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
-				],
-		'sum': 122,
-		'first-page': True,
-		'last-page': False,
-		'sum-before': 0
-		},
-		{'items':
-			[
-				{'name': 'Kommission 1'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
-				{'name': 'Kommission 2'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
-				{'name': 'Kommission 3'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
-			],
-			'sum': 334,
-			'first-page': False,
-			'last-page': False,
-			'sum-before': 122
-		},
-		{'items':
-			[
-				{'name': 'Kommission 1'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
-				{'name': 'Kommission 2'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
-				{'name': 'Kommission 3'},
-				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
-				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
-				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
-			],
-			'sum': 122,
-			'first-page': False,
-			'last-page': True,
-			'sum-before': 334
-		}
-	],
-	'location': 'Tragwein',
-	'date': '15.06.2017',
-	'receiver': 'Patrick Moertenboeck',
-	'address': 'Gartenstrasse 12',
-	'city': 'Tragwein',
-	'bill_no': 'TR231',
-	'discount': -10.15
-}
 
 # price_info = get_total_price(context)
 #
@@ -243,16 +162,82 @@ with open("bill.html", "w") as f:
 import pdfkit
 options = {
     'page-size': 'A4',
-    # 'margin-top': '0.75in',
-    # 'margin-right': '0.75in',
-    # 'margin-bottom': '0.75in',
-    # 'margin-left': '0.75in',
     'encoding': "UTF-8",
-    # 'custom-header' : [
-    #     ('Accept-Encoding', 'gzip')
-    # ],
-    # 'no-outline': None,
 	'dpi': 400,
 	'header-right':'[page]/[toPage]'
 }
 pdfkit.from_url('file:///Users/Moerti/AA_projects/bill-creator/bill.html', 'out.pdf', options=options)
+
+
+### so soll context aussehen
+# context = {
+# 	'pages':[
+# 		{'items':
+# 				[
+# 				{'name': 'Kommission 1'},
+# 				{'amount':5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
+# 				{'amount':5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount':5,'unit':'lfm','text':'ziehen','price_per_unit':2,'price':10},
+# 				{'name': 'Kommission 2'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
+# 				{'name': 'Kommission 3'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
+# 				],
+# 		'sum': 122,
+# 		'first-page': True,
+# 		'last-page': False,
+# 		'sum-before': 0
+# 		},
+# 		{'items':
+# 			[
+# 				{'name': 'Kommission 1'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
+# 				{'name': 'Kommission 2'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
+# 				{'name': 'Kommission 3'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
+# 			],
+# 			'sum': 334,
+# 			'first-page': False,
+# 			'last-page': False,
+# 			'sum-before': 122
+# 		},
+# 		{'items':
+# 			[
+# 				{'name': 'Kommission 1'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store nähen', 'price_per_unit': 10, 'price': -50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
+# 				{'name': 'Kommission 2'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10},
+# 				{'name': 'Kommission 3'},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'Store naehen', 'price_per_unit': 10, 'price': 50},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'FB', 'price_per_unit': 1, 'price': 5},
+# 				{'amount': 5, 'unit': 'lfm', 'text': 'ziehen', 'price_per_unit': 2, 'price': 10}
+# 			],
+# 			'sum': 122,
+# 			'first-page': False,
+# 			'last-page': True,
+# 			'sum-before': 334
+# 		}
+# 	],
+# 	'location': 'Tragwein',
+# 	'date': '15.06.2017',
+# 	'receiver': 'Patrick Moertenboeck',
+# 	'address': 'Gartenstrasse 12',
+# 	'city': 'Tragwein',
+# 	'bill_no': 'TR231',
+# 	'discount': -10.15
+# }
